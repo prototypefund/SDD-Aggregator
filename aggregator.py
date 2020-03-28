ad55a9755e45775ef5f3c607de6a2934db538d46
Walther <thomas.walther01@sap.com>
2020-03-28 21:52:56 +0100

    first commit

---
+++ aggregator.py
@@ -0,0 +1,104 @@
+import boto3
+import pandas as pd
+from datetime import date, timedelta
+from agg_webcam import aggregate as agg_webcam
+from agg_hystreet import aggregate as agg_hystreet
+from agg_gmap_supermarket_score import aggregate as agg_gmap_supermarket_score
+from agg_gmap_transit_score import aggregate as agg_gmap_transit_score
+from agg_zugdaten import aggregate as agg_zugdaten
+from agg_fahrrad import aggregate as agg_fahrrad
+from agg_airquality import aggregate as agg_airquality
+from agg_lemgo_digital import aggregate as agg_lemgo_digital
+from agg_tomtom import aggregate as agg_tomtom
+import json
+
+#How far back do you want to aggregate data?
+days = 4
+
+s3_client = boto3.client('s3')
+for x in range(0,days):
+    date = date.today() - timedelta(days = x)
+    print(date)
+    list_result = pd.DataFrame(columns = ['landkreis'])
+    list_result = list_result.set_index("landkreis")
+
+    try:
+        lemgo_digital_list = pd.DataFrame(agg_lemgo_digital(date))
+        lemgo_digital_list = lemgo_digital_list.set_index('landkreis')
+        list_result = list_result.join(lemgo_digital_list, how="outer")
+    except Exception as e:
+        print(e)
+
+    try:
+        gmapscore_list = pd.DataFrame(agg_gmap_transit_score(date))
+        gmapscore_list = gmapscore_list.set_index('landkreis')
+        list_result = list_result.join(gmapscore_list, how="outer")
+    except Exception as e:
+        print("Error GMAP:")
+        print(e)
+
+    try:
+        gmapscore_supermarket_list = pd.DataFrame(agg_gmap_supermarket_score(date))
+        gmapscore_supermarket_list = gmapscore_supermarket_list.set_index('landkreis')
+        list_result = list_result.join(gmapscore_supermarket_list, how = "outer")
+    except Exception as e:
+        print("Error GMAP Super")
+        print(e)
+    try:
+        webcam_list = pd.DataFrame(agg_webcam(date))
+        webcam_list = webcam_list.set_index('landkreis')
+        list_result = list_result.join(webcam_list, how="outer")
+    except Exception as e:
+        print("Error Webcam")
+        print(e)
+
+    try:
+        hystreet_list = pd.DataFrame(agg_hystreet(date))
+        hystreet_list = hystreet_list.set_index('landkreis')
+        list_result = list_result.join(hystreet_list, how = "outer")
+    except Exception as e:
+        print("Error Hystreet")
+        print(e)
+
+    try:
+        zugdaten_list = pd.DataFrame(agg_zugdaten(date))
+        zugdaten_list = zugdaten_list.set_index('landkreis')
+        list_result = list_result.join(zugdaten_list, how="outer")
+    except Exception as e:
+        print("Error Zugdaten")
+        print(e)
+
+    try:
+        fahrrad_list = pd.DataFrame(agg_fahrrad(date))
+        fahrrad_list = fahrrad_list.set_index('landkreis')
+        list_result = list_result.join(fahrrad_list, how="outer")
+    except Exception as e:
+        print("Error Fahrrad")
+        print(e)
+
+    # try:
+    #     airquality_list = pd.DataFrame(agg_airquality(date))
+    #     airquality_list = airquality_list.set_index('landkreis')
+    #     list_result = list_result.join(airquality_list, how="outer")
+    # except Exception as e:
+    #     print("Error Airquality")
+    #     print(e)
+
+    try:
+        tomtom_list = pd.DataFrame(agg_tomtom(date))
+        tomtom_list = tomtom_list.set_index('landkreis')
+        list_result = list_result.join(tomtom_list, how="outer")
+    except Exception as e:
+        print("Error Tomtom")
+        print(e)
+
+    list_result["date"] = str(date)
+    list_result.to_csv("test.csv")
+
+    list_result
+    dict = list_result.T.to_dict()
+    dict
+    # s3_client.put_object(Bucket='sdd-s3-basebucket', Key="aggdata/live", Body=json.dumps(dict))
+    response = s3_client.put_object(Bucket='sdd-s3-basebucket', Key='aggdata/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2),
+                                                                  str(date.day).zfill(2)), Body=json.dumps(dict))
+    print(response)
---
+++ export.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+import datetime
+import json
+import numpy as np
+
+#Specify Timeframe
+min_date = datetime.datetime.now().date() - datetime.timedelta(days=2)
+max_date = datetime.datetime.now().date()
+params = {"min_date": str(min_date), "max_date": str(max_date), "data_sources":"0,1,2"}
+response = requests.get('https://f3fp7p5z00.execute-api.eu-central-1.amazonaws.com/test/sdd-lambda-request',params = params)
+response.json()
+data = pd.DataFrame()
+for day, data_day in json.loads(response.json()["body"])["body"].items():
+    # if day == "2020-03-23":
+    #     continue
+    daily_data = pd.DataFrame.from_dict(data_day, orient='index')
+    daily_data = daily_data.reset_index()
+    daily_data["date"] = day
+    data = data.append(daily_data)
+
+
+data.columns
+# 'index': AGS-ID of Landkreis
+# 'date': Date of measurement
+# 'gmap_score': How many people are at transit stations compared to normal day?
+# 'hystreet_score': How many people are walking by hystreet sensors compared to normal day?
+# 'nationalExpress_score': 'national_score', 'regional_score','suburban_score', 'bus_score', 'zug_score': How many connections got cancelled?
+# "bike_score": How many people were travelling on bikes that day compared to a normal day? Here someone used fancy machine learning to cancel out the effect of weather.
+# 'webcam_score': How many people are visible on webcams in public places divided by 2.4 (->we dont have a "normal" value here so we use 1/highscore median)
+
+data.replace(np.inf, np.nan, inplace=True)
+data.loc[data["hystreet_score"].notna()]["date"].value_counts()
+aggregate = data.groupby("date")[['bike_score', 'bus_score', 'gmap_score', 'hystreet_score', 'nationalExpress_score', 'national_score', 'regional_score','suburban_score', 'webcam_score', 'zug_score']].mean()
+pd.DataFrame(aggregate).to_csv("aggregate.csv")
+data.to_csv("export.csv")
