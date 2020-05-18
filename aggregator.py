759a9e012fd75205120d6fccc19bbb89f843348b
Michael Adams <github@mmmad.de>
2020-05-18 21:17:12 +0100

    Better log messages

--- aggregator.py
+++ aggregator.py
@@ -20,10 +20,12 @@ s3_client = boto3.client('s3')
 
 for x in range(0,days):
     date = date.today() - timedelta(days = x)
-    print(date)
+    print("\n##########################")
+    print('###   START ',date,"\n")
     list_result = pd.DataFrame(columns = ['landkreis'])
     list_result = list_result.set_index("landkreis")
-
+    
+    print("start lemgo...")
     try:
         lemgo_digital_list = pd.DataFrame(agg_lemgo_digital(date))
         lemgo_digital_list = lemgo_digital_list.set_index('landkreis')
@@ -31,7 +33,9 @@ for x in range(0,days):
     except Exception as e:
         print("Error Lemgo:")
         print(e)
-
+    
+    print("--------------")
+    print("start gmap...")
     try:
         gmapscore_list = pd.DataFrame(agg_gmap_transit_score(date))
         gmapscore_list = gmapscore_list.set_index('landkreis')
@@ -40,13 +44,8 @@ for x in range(0,days):
         print("Error GMAP:")
         print(e)
 
-    # try:
-    #     gmapscore_supermarket_list = pd.DataFrame(agg_gmap_supermarket_score(date))
-    #     gmapscore_supermarket_list = gmapscore_supermarket_list.set_index('landkreis')
-    #     list_result = list_result.join(gmapscore_supermarket_list, how = "outer")
-    # except Exception as e:
-    #     print("Error GMAP Super")
-    #     print(e)
+    print("--------------")
+    print("start webcams...")
     try:
         webcam_list = pd.DataFrame(agg_webcam(date))
         webcam_list = webcam_list.set_index('landkreis')
@@ -54,7 +53,8 @@ for x in range(0,days):
     except Exception as e:
         print("Error Webcam")
         print(e)
-
+    print("--------------")
+    print("start hystreet...")
     try:
         hystreet_list = pd.DataFrame(agg_hystreet(date))
         hystreet_list = hystreet_list.set_index('landkreis')
@@ -63,14 +63,8 @@ for x in range(0,days):
         print("Error Hystreet")
         print(e)
 
-    # try:
-        # zugdaten_list = pd.DataFrame(agg_zugdaten(date))
-        # zugdaten_list = zugdaten_list.set_index('landkreis')
-        # list_result = list_result.join(zugdaten_list, how="outer")
-    # except Exception as e:
-        # print("Error Zugdaten")
-        # print(e)
-
+    print("--------------")
+    print("start fahrrad...")
     try:
         fahrrad_list = pd.DataFrame(agg_fahrrad(date))
         fahrrad_list = fahrrad_list.set_index('landkreis')
@@ -78,7 +72,9 @@ for x in range(0,days):
     except Exception as e:
         print("Error Fahrrad")
         print(e)
-
+    
+    print("--------------")
+    print("start airquality...")
     try:
         airquality_list = agg_airquality(date)
         if airquality_list == []:
@@ -90,7 +86,9 @@ for x in range(0,days):
     except Exception as e:
         print("Error Airquality")
         print(e)
-
+        
+    print("--------------")
+    print("start tomtom...")
     try:
         tomtom_list = pd.DataFrame(agg_tomtom(date))
         tomtom_list = tomtom_list.set_index('landkreis')
@@ -99,13 +97,16 @@ for x in range(0,days):
         print("Error Tomtom")
         print(e)
 
+    print("--------------")
+    print("write output...")
     list_result["date"] = str(date)
-    list_result.to_csv("test.csv")
+    #list_result.to_csv("test.csv")
 
-    list_result
+    #list_result
     dict = list_result.T.to_dict()
-    dict
+    #dict
     # s3_client.put_object(Bucket='sdd-s3-basebucket', Key="aggdata/live", Body=json.dumps(dict))
-    response = s3_client.put_object(Bucket=settings.BUCKET, Key='aggdata/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2),
-                                                                  str(date.day).zfill(2)), Body=json.dumps(dict))
-    print(response)
+    response = s3_client.put_object(Bucket=settings.BUCKET, Key='aggdata/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2),str(date.day).zfill(2)), Body=json.dumps(dict))
+    print("s3_client.put_object response:",response)
+    print('\n###     END ',date,"")
+    print("##########################\n")
