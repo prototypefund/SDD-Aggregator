c153b9128636c84973fc3161fa4ea26b664a5fdc
Julius <42575200+BihJ@users.noreply.github.com>
2020-07-28 18:01:12 +0200

    print delete

--- agg_fahrrad.py
+++ agg_fahrrad.py
@@ -73,10 +73,10 @@ def aggregate(date):
     return list_results
     
    
-if __name__ == '__main__':
-    # for testing
-    for i in range(1,14):
-        date = date.today() - timedelta(days = i)
-        list_results = aggregate(date)
-    print(list_results)
-    
+# if __name__ == '__main__':
+#     # for testing
+#     for i in range(1,14):
+#         date = date.today() - timedelta(days = i)
+#         list_results = aggregate(date)
+#     # print(list_results)
+#
