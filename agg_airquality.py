96ac71cd6acca120b7e44d5d65f898d603e66528
Julius <42575200+BihJ@users.noreply.github.com>
2020-07-29 23:02:50 +0200

    airquality fix

--- agg_airquality.py
+++ agg_airquality.py
@@ -19,21 +19,27 @@ def aggregate(date_obj):
         jsonItem = s3Handler.readFromAWS(pathItem)
         if jsonItem != False:
             fullData = fullData + jsonItem
+    df = Aggregation.Aggregator.aggregateJson(fullData,"airquality","aqi","airquality_score")
 
     # Aggregation.Aggregator.aggregateDf(fullData, "airquality", "aqi", "airquality_score")
     # pd.DataFrame.from_records(fullData)
     list_fields = ["airquality_score", "lat", "lon"]
-    list_tags = ["state", "landkreis", "districtType", "ags"] #"name", "_id", "origin"
+    list_tags = ["state", "landkreis", "districtType", "_id", "ags"] #"name",  "origin"
 
-    df = Aggregation.Aggregator.aggregateJson(fullData,"airquality","aqi","airquality_score")
+    df[["lat","lon"]] = df[["lat","lon"]].astype(float)
+    df = df.sort_values(by=["lat", "lon"])
+    df["_id"] = range(len(df))
     df["measurement"] = "airquality"
+    # df["_id"] = \
+    # len(pd.unique((df["lon"] * 1000).astype(int).astype(str) + (df["lon"] * 1000).astype(int).astype(str)))
+
     # df["measurement"] = "airquality"
     # df["time"] = pd.to_datetime(date)
     # df["time"] = pd.to_datetime(date).timestamp()
-    df["time"] = datetime.datetime(year=date_obj.year, month=date_obj.month, day=date_obj.day, hour=12).isoformat()
+    # df["time"] = datetime.datetime(year=date_obj.year, month=date_obj.month, day=date_obj.day, hour=12).isoformat()
     # df["time"] = date.hour
     # df["time"] = datetime.datetime.timestamp(year=date.year, month=date.month, day=date.day)
-
+    # df["time"] = df["datetime"]
     list_jsons = convert_df_to_influxdb(df, list_tags=list_tags, list_fields=list_fields)
     push_to_influxdb(list_jsons)
     return json.loads(df.to_json(orient='records'))
